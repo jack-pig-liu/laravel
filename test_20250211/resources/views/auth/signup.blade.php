6ff4d7f90d9a33ab30dccde5016b639d2be29f0d
jack liu <jack.liu@tkspring.com>
2025-02-13 11:48:22 +0800

    建立註冊表單 包含CSRF保護

--- test_20250211/resources/views/auth/signup.blade.php
+++ test_20250211/resources/views/auth/signup.blade.php
@@ -7,9 +7,16 @@
 <!-- 傳送資料到母模板，並指定變數為 content -->
 @section('content')
 <h1>{{ $title }}</h1>
-@include('component.social')
-Email： <input type="text" name="email" placeholder="Email">
-密碼： <input type="password" name="password" placeholder="密碼">
-暱稱： <input type="text" name="nickname" placeholder="暱稱">
-@endsection
 
+<!-- @include('component.social') -->
+
+<form action="/user/auth/signup" method="post">
+    {{ csrf_field() }}
+    暱稱： <input type="text" name="nickname" placeholder="暱稱"><br>
+    Email： <input type="text" name="email" placeholder="Email"><br>
+    密碼： <input type="password" name="password" placeholder="密碼"><br>
+    使用者類型： <input type="radio" name="usertype" value="user"> 一般使用者
+    <input type="radio" name="usertype" value='admin'> 管理員<br>
+    <input type="submit" value="註冊">
+</form>
+@endsection
